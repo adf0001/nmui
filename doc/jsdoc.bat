eda07eda5e30eeb641db70b0d6983ec585d9a6e6
fwg <adf0001@163.com>
2018-09-06 22:56:55 +0800

    improve jsdoc style

--- doc/jsdoc.bat
+++ doc/jsdoc.bat
@@ -3,6 +3,8 @@
 
 set jsdocPath="C:\Users\Administrator\AppData\Roaming\npm\jsdoc.cmd"
 
-%jsdocPath% -c .\jsdoc.conf.json
+call %jsdocPath% -c .\jsdoc.conf.json
 
-@rem pause
+copy .\out\styles\jsdoc-default.css+.\jsdoc-extra.css .\out\styles\jsdoc-default.css
+
+pause
